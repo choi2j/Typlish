b49128620c597e22354d8f1f9828b1bfe2d685a6
최이준 <choi2j@choeijun-ui-MacBookAir.local>
2022-05-29 14:24:34 +0900

    first commit

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+const MongoClient = require('mongodb').MongoClient;
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 app.set('view engine', 'ejs');
 
+app.use('/public', express.static('public'));
 
-app.listen('8080', () => {
-    console.log('listening on 8080');
+app.listen('7070', () => {
+    console.log('listening on 7070');
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + './html/index.html');
+    res.render('index.ejs');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.get('/home', (req, res) => {
+    res.render('login.ejs');
 });
