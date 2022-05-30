590e5f81df3124892cbc388fcb31e31d74ec036d
최이준 <choi2j@choeijun-ui-MacBookAir.local>
2022-05-30 22:37:07 +0900

    first commit

--- server.js
+++ server.js
@@ -14,6 +14,18 @@ app.listen('7070', () => {
     console.log('listening on 7070');
 });
 
+// var db;
+
+// MongoClient.connect('mongodb+srv://choi2j:1234@cluster0.qjbln.mongodb.net/todoApp?retryWrites=true&w=majority', function(err, client) {
+//     if (err) return console.log(err);
+
+//     db = client.db('todoapp');
+
+//     app.listen('8080', () => {
+//         console.log('listening on 8080');
+//     });
+// })
+
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
@@ -27,5 +39,26 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/home', (req, res) => {
-    res.render('login.ejs');
+    res.render('home.ejs');
+});
+
+app.get('/info/:name', (req, res) => {
+    db.collection('user').findOne({ _id: parseInt(req.params.id) }, (err, result) => {
+        console.log(result);
+        res.render('info.ejs', { post: result });
+    });
+});
+
+app.get('/paragraph/:id', (req, res) => {
+    db.collection('book').findOne({ _id: parseInt(req.params.id) }, (err, result) => {
+        console.log(result);
+        res.render('paragraph.ejs', { post: result });
+    });
+});
+
+app.get('/typing/:id', (req, res) => {
+    db.collection('book').findOne({ _id: parseInt(req.params.id) }, (err, result) => {
+        console.log(result);
+        res.render('typing.ejs', { post: result });
+    });
 });
